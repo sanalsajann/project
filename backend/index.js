093acd82a719a159db1c4cffc441e9ae7829fdd2
Avin Varghese Mathew <avinvmavolil@gmail.com>
2024-08-03 23:25:33 +0530

    Minor changes in cards style and backend for cards

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const PORT = 4002;
 const eventModel = require('./model/eventData');
+const cardModel = require('./model/cardData');
 require('./connection');
 app.use(cors());
 app.use(express.json());
@@ -31,6 +32,19 @@ app.post('/events', (req, res) => {
       res.status(500).json({ message: "Internal Server Error" });
     });
 });
+
+
+// Define the GET route to fetch events
+app.get('/eventdeets', (req, res) => {
+  cardModel.find({})
+    .then((events) => res.json(events))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`); // use template literals
--- backend/model/eventData.js
+++ backend/model/eventData.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+
 const eventSchema=mongoose.Schema({
     username:String,
     email:String,
