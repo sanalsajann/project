2b60a7db43c3f9dcf6ef6588521b40d3a2b95d64
sanal <sanalsajan916@gmail.com>
2024-08-08 22:15:54 +0530

    Git

--- backend/model/eventData.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const eventSchema=mongoose.Schema({
-    username:String,
-    email:String,
-    password:String,
-})
-const EventData=mongoose.model('signups',eventSchema,);
-module.exports=EventData
